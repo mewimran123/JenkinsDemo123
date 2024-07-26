5b8e5ef0d35c8b22ac9b45d45ed6d9c91a07e29b
mewimran123 <mew.imran@gmail.com>
2024-07-26 09:19:18 +0000

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  access_key = ""
+  secret_key = ""
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "demo1" {
+    cidr_block = "192.168.0.0/16"
+    tags = {
+        "Name" = "VPC1_NTS28"
+    }
+  
+}
